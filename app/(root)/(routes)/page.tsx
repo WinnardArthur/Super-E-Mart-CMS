58d346993e9b7590c238804442d66af15741853b
Winnard Arthur <winnardarthur7@gmail.com>
2023-12-02 17:54:19 +0000

    fetch store and redirect to specifi stores

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,22 +1,18 @@
-'use client';
+"use client";
 
 import { useEffect } from "react";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function SetupPage() {
-  const onOpen = useStoreModal(state => state.onOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
   useEffect(() => {
-    if(!isOpen) {
+    if (!isOpen) {
       onOpen();
     }
-  }, [isOpen, onOpen])
+  }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      Root Page
-    </div>
-  );
+  return null;
 }
