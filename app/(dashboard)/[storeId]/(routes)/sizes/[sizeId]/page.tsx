0e9e6bd20aaf1880e9711827df5b6947d8400bfe
Winnard Arthur <winnardarthur7@gmail.com>
2023-12-07 11:23:14 +0000

    create size pagg

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import React from "react";
+import { SizeForm } from "./components/size-form";
+
+const SizePage = async ({
+  params,
+}: {
+  params: { sizeId: string };
+}) => {
+  let size = null;
+  try {
+    size = await prismadb.size.findUnique({
+      where: {
+        id: params.sizeId,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 spacey-y-4 p-8">
+        <SizeForm initialData={size} />
+      </div>
+    </div>
+  );
+};
+
+export default SizePage;
